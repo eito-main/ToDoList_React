63f96d69f7dea73ba36ccda54f86d6098df3183a
sagaraEito <gjmptw.aab@gmail.com>
2022-07-18 18:38:49 +0900

    first commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+function ToDoList() {
+	const [todos, setTodos] = useState([])
+	const [todo, setTodo] = useState("")
+
+	function handlePush() {
+		if (todo !== "") {
+			setTodos(todos.concat([todo]))
+			setTodo("")
+		}
+	}
+	return (
+		<div>
+            <h1>ToDoリスト</h1>
+			<input value={todo} onChange={(e) => setTodo(e.target.value)} />
+			<button onClick={handlePush}>登録</button>
+            <ul>
+				{todos.map((todo, index) => (
+					<li key={index}>{todo}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default ToDoList
